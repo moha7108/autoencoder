a68e148e76526199de64e493add3d7fddc4071aa
moha7108 <moha7108@protonmail.com>
2023-02-26 19:26:33 -0500

    load mnist data

--- autoencoder.py
+++ autoencoder.py
@@ -1,3 +1,12 @@
 import numpy
 import torch
 import matplotlib.pyplot as plt
+
+from torchvision import datasets, transforms
+
+
+transform = transforms.ToTensor()
+
+mnist = datasets.MNIST(root ='./data', train = True, download= True, transform=transform)
+
+data_loader = torch.utils.data.DataLoader(dataset=mnist, batch_size=64, shuffle=True)
